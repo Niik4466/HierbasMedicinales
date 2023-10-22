d27bb035dc4fa0e8be9615cc31447cb556f34b23
ivan0pium23 <ivan_.duran@icloud.com>
2023-10-22 13:37:54 -0300

    Cambio de tamaño del drawer a xl

--- app/MainPage.js
+++ app/MainPage.js
@@ -44,14 +44,22 @@ export default function Home({data}) {
             placement='right'
             onClose={onClose}
             finalFocusRef={btnRef}
+            size = {"xl"}
           >
             <DrawerOverlay />
             <DrawerContent>
               <DrawerCloseButton />
-              <DrawerHeader>Create your account</DrawerHeader>
-
+              <DrawerHeader>Selecciona tipo de dolencia</DrawerHeader>
               <DrawerBody>
-
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Consequat nisl vel pretium lectus quam id. Semper quis lectus
+                nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus
+                quis varius quam quisque. Massa ultricies mi quis hendrerit dolor
+                magna eget est lorem. Erat imperdiet sed euismod nisi porta.
+                Lectus vestibulum mattis ullamcorper velit.
+                </p>
               </DrawerBody>
 
               <DrawerFooter>
