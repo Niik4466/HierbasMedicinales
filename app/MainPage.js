dd21cef9237462776d0524bdd230618cafb1b22f
ivan0pium23 <ivan_.duran@icloud.com>
2023-10-22 13:21:54 -0300

    Drawer Implementado

--- app/MainPage.js
+++ app/MainPage.js
@@ -1,6 +1,20 @@
 "use client"
 
-import { Card, CardBody, CardFooter, CardHeader, Container, Text, chakra, extendTheme, ChakraProvider, Img, Button, ButtonGroup} from "@chakra-ui/react";
+import {
+  Text,
+  extendTheme,
+  ChakraProvider,
+  Button,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+import React from 'react';
 import TarjetaHierba from "./components/TarjetaHierba";
 
 const theme = extendTheme({
@@ -10,14 +24,44 @@ const theme = extendTheme({
   },
 });
 
+
+
+
 export default function Home({data}) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
   return(
     <ChakraProvider theme={theme}>
       <div>
           <Text fontSize="5xl" marginBottom={"2%"} maxWidth={"75%"} marginLeft={"1rem"}>
             Catálogo de hierbas medicinales Chilenas
           </Text>
-          <Button colorScheme='blue' justifyContent="left">Filtrar por dolencia</Button>
+          <Button ref={btnRef} colorScheme='blue' justifyContent="left" onClick={onOpen}>Filtrar por dolencia</Button>
+          <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Create your account</DrawerHeader>
+
+              <DrawerBody>
+
+              </DrawerBody>
+
+              <DrawerFooter>
+                <Button variant='outline' mr={3} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme='blue'>Save</Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
           <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", flexWrap:"wrap", margin:"1rem"}}>
             {data.map((hierba) => (
               <TarjetaHierba hierba={hierba}/>
---
+++ app/components/DrawerDolencias.js
@@ -0,0 +1,11 @@
+"use client"
+
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+  } from '@chakra-ui/react'
