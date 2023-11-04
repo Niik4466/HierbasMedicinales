160f31daa22ad9b3b63afc14ad47221014a6a3c6
kaihavxd <eduardo.barreran@gmail.com>
2023-11-04 18:00:06 -0300

    Cambiar la lista de dolencias por botones

--- app/components/DrawerComponent.js
+++ app/components/DrawerComponent.js
@@ -10,9 +10,17 @@ import {
   Button,
 
   Box,
+  Stack,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 
+const CustomButton = ({ children, ...props }) => {
+  return (
+    <Button variant='link' size='xl' {...props}>
+      {children}
+    </Button>
+  );
+};
 
 const DrawerComponent = ({ isOpen, onClose }) => {
   return (
@@ -23,17 +31,38 @@ const DrawerComponent = ({ isOpen, onClose }) => {
         <DrawerCloseButton />
         <DrawerHeader style={{ fontSize: '35px' }} >Selecciona tipo de dolencia</DrawerHeader>
         <DrawerBody className="drawer-text">
-          <p > Problemas renales y de las vias urinarias </p>
-          <p > Afecciones a la piel y lesiones externas </p>
-          <p > Problemas y malestares estomacales </p>
-          <p > Dolor corporal y de las articulaciones </p>
-          <p > Problemas hepaticos y/o biliares </p>
-          <p > Fiebre y resfriado </p>
-          <p > Problemas menstruales </p>
-          <p > Dolores de cabeza </p>
-          <p > Salud mental </p>
-          <p > Enfermedades crónicas </p>
-          <p > </p>
+          <Stack spacing={4} align="flex-start">
+            <CustomButton>
+              Problemas renales y de las vias urinarias 
+            </CustomButton>
+            <CustomButton> 
+              Afecciones a la piel y lesiones externas 
+            </CustomButton>
+            <CustomButton >
+              Problemas y malestares estomacales
+            </CustomButton >
+            <CustomButton>
+              Dolor corporal y de las articulaciones 
+            </CustomButton>
+            <CustomButton>
+              Problemas hepaticos y/o biliares 
+            </CustomButton>
+            <CustomButton>
+              Fiebre y resfriado 
+            </CustomButton>
+            <CustomButton>
+              Problemas menstruales 
+            </CustomButton>
+            <CustomButton>
+              Dolores de cabeza 
+            </CustomButton>
+            <CustomButton>
+              Salud mental 
+            </CustomButton>
+            <CustomButton>
+              Enfermedades crónicas 
+            </CustomButton>
+          </Stack>
         </DrawerBody>
         <DrawerFooter>
           <Button variant="outline" mr={3} onClick={onClose}>
