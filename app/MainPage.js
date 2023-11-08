ad33e578e2a4f46f79e2542bfc8cd9ac2bf340e7
Alex <alex.garnica@alumnos.uach.cl>
2023-11-07 22:04:12 -0300

    Agregando libreria de iconos react-icons

--- app/MainPage.js
+++ app/MainPage.js
@@ -1,12 +1,6 @@
-"use client"
+"use client";
 
-import {
-  Text,
-  extendTheme,
-  ChakraProvider,
-  Button,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Text, extendTheme, ChakraProvider, Button, useDisclosure } from "@chakra-ui/react";
 import React, { useRef } from "react";
 import TarjetaHierba from "./components/TarjetaHierba";
 import { useState } from "react";
@@ -20,28 +14,37 @@ const theme = extendTheme({
   },
 });
 
-export default function Home({data}) {
-
+export default function Home({ data }) {
   const btnRef = useRef();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [dolencia, setDolencia] = useState("None");
 
-  return(
+  return (
     <ChakraProvider theme={theme}>
       <div>
-          <Text fontSize="5xl" marginBottom={"2%"} maxWidth={"75%"} marginLeft={"1rem"}>
-            Catálogo de hierbas medicinales Chilenas
-          </Text>
-          <Button ref={btnRef} colorScheme="teal" marginLeft={"1rem"} onClick={onOpen}>Filtrar por dolencia</Button>
-          <DrawerComponent isOpen={isOpen} onClose={onClose} setDolencia={setDolencia}/>
-          <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", flexWrap:"wrap", margin:"1rem"}}>
-            {data.map((hierba) => (
-              <TarjetaHierba hierba={hierba} dolencia={dolencia} setDolencia={setDolencia}/>
-            ))}
-            {console.log(dolencia)}
-          </div>
-      </div>  
-      <SmallCentered/>
+        <Text fontSize="5xl" marginBottom={"2%"} maxWidth={"75%"} marginLeft={"1rem"}>
+          Catálogo de hierbas medicinales Chilenas
+        </Text>
+        <Button ref={btnRef} colorScheme="teal" marginLeft={"1rem"} onClick={onOpen}>
+          Filtrar por dolencia
+        </Button>
+        <DrawerComponent isOpen={isOpen} onClose={onClose} setDolencia={setDolencia} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            margin: "1rem",
+          }}
+        >
+          {data.map((hierba, index) => (
+            <TarjetaHierba key={index} hierba={hierba} dolencia={dolencia} setDolencia={setDolencia} />
+          ))}
+          {console.log(dolencia)}
+        </div>
+      </div>
+      <SmallCentered />
     </ChakraProvider>
-  )
+  );
 }
