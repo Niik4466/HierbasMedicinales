d6d75f0fa5c3f92e12a01356757e76369c71bdc2
ivan0pium23 <ivan_.duran@icloud.com>
2023-11-15 12:25:34 -0300

    se borró el footer

--- app/components/DrawerComponent.js
+++ app/components/DrawerComponent.js
@@ -52,7 +52,7 @@ const DrawerComponent = ({ isOpen, onClose, setDolencia}) => {
         <DrawerHeader style={{ fontSize: '40px' }} >Selecciona tipo de dolencia</DrawerHeader>
         <DrawerBody className="drawer-text">
         <Stack spacing={4} align={"flex-start"}>
-          <CustomButton on onClick={() => handleClick("Todas")}>Mostrar todas</CustomButton>
+          <CustomButton onClick={() => handleClick("Todas")}>Mostrar todas</CustomButton>
           {dolencias.map((dolencia, index) => (
             <CustomButton key={index} onClick={() => handleClick(dolencia)}>
               {dolencia}
@@ -60,9 +60,6 @@ const DrawerComponent = ({ isOpen, onClose, setDolencia}) => {
           ))}
         </Stack>
         </DrawerBody>
-        <DrawerFooter>
-          <Button colorScheme="blue" style={{ fontSize: '35px' }} onClick={() => handleClick("Todas")}>Limpiar</Button>
-        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
