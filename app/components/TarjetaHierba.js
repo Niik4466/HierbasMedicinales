8b27a44cbc56cf17f702baea506fe49bec5e162c
lombrices <claudiavxd@gmail.com>
2023-11-22 09:42:58 -0300

    Scroll en el card y separacion de preparacion y dolencia

--- app/components/TarjetaHierba.js
+++ app/components/TarjetaHierba.js
@@ -13,7 +13,7 @@ export default function TarjetaHierba({hierba, dolencia, setDolencia}){
     if (hierba.dolencia.includes(dolencia) || dolencia == "Todas"){
 
         return(
-            <Card as='button' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} marginBottom={"2.5%"} border="solid" borderWidth={"1%"} marginLeft={"1rem"} width={"20em"} height={"24em"} maxHeight={"24em"} marginRight={"1rem"} onClick={handleClick} >
+            <Card as='button' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} marginBottom={"2.5%"} border="solid" borderWidth={"1%"} marginLeft={"1rem"} width={"20em"} height={"24em"} maxHeight={"24em"} marginRight={"1rem"} onClick={handleClick} overflow={"auto"}>
                 <div style={{transform: !flip ? "rotateY(180deg)" : "rotateY(0deg)", transition: "transform 1s", width: "100%", height: "100%", position: "absolute", backfaceVisibility: "hidden",}}>
                     <Img src={hierba.foto} fit={"fill"} boxSize={"100%"} width={"20em"} height={"16.5em"} alt={` Imagen de ${hierba.planta}`} />
                     <CardBody marginBottom={"-10%"}>
@@ -42,10 +42,19 @@ export default function TarjetaHierba({hierba, dolencia, setDolencia}){
                         style={{ transform: "rotateY(0deg)", transition: "transform 1s", width: "100%", height: "100%", position: "absolute", backfaceVisibility: "hidden",}}>
                         <CardBody>
                             <Text fontSize={"x-large"} align={"center"}>
-                                {hierba.planta}    
+                                <strong>
+                                {hierba.planta} 
+                                </strong>
+                                   
                             </Text>
                             <Text >
                                 {hierba.informacion}
+                                <br></br>
+                                <br></br>
+                                <strong >
+                                    Usos: 
+                                    </strong>
+                                {hierba.usos}
                             </Text>
                         </CardBody>
                     </div>
